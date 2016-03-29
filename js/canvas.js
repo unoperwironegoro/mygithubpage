5cd75dbc6804ee107e0ea9c47cbe37414885023c
Uno <hperwironegoro@gmail.com>
2016-03-29 17:34:17 +0100

    Apparently the bugs are from the buttons themselves

--- js/canvas.js
+++ js/canvas.js
@@ -310,11 +310,11 @@ var axisLength = 10;
 
 function resetCanvas() {
   clearCanvas();
+  populateCanvas();
 }
 
 function clearCanvas() {
-  ECS.Entities.length = 0;
-  populateCanvas();
+  ECS.Entities = [];
 }
 
 function createCanvas() {
